2446758a10a65c47ee2e2b32f3ad5bb82bb9f720
ronald.assis <ronaldassis71@gmail.com>
2024-04-04 09:10:49 -0300

    feat(home-page): created card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,38 @@
 import { Card } from '@/components/Card'
+import { Header } from '@/components/Header'
+import dynamicIconImports from 'lucide-react/dynamicIconImports'
+
+type PropToCardType = {
+  title: string
+  icon: keyof typeof dynamicIconImports
+  sizeIcon: number
+}
 
 export default function Home() {
+  const titles: PropToCardType[] = [
+    {
+      title: 'Tipologia de Fraudes em Licitações e Contratos',
+      icon: 'graduation-cap',
+      sizeIcon: 88,
+    },
+    {
+      title: 'Predições',
+      icon: 'graduation-cap',
+      sizeIcon: 88,
+    },
+    {
+      title: 'Indicadores de Políticas Publicas',
+      icon: 'graduation-cap',
+      sizeIcon: 88,
+    },
+  ]
+
   return (
-    <main className="relative mt-20 flex min-h-screen items-center justify-center bg-gray-200">
-      <Card
-        title="Tipologia de Fraudes em Licitações e Contratos"
-        sizeIcon={88}
-      />
+    <main className="relative flex min-h-screen items-center justify-center gap-6 bg-gray-200">
+      <Header />
+      {titles.map((t, i) => (
+        <Card key={i} title={t.title} sizeIcon={t.sizeIcon} icon={t.icon} />
+      ))}
     </main>
   )
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,20 +1,17 @@
+import dynamicIconImports from 'lucide-react/dynamicIconImports'
 import { Icon } from '../Icon'
 
 type CardType = {
   title: string
   sizeIcon: number
+  icon: keyof typeof dynamicIconImports
 }
 
-export function Card({ title, sizeIcon }: CardType) {
+export function Card({ title, sizeIcon, icon }: CardType) {
   return (
     <div className="flex h-72 w-96 flex-col items-center justify-center rounded-lg bg-blue_warm-70 text-white shadow-2xl transition duration-300 hover:-translate-y-2 hover:scale-105 hover:bg-blue_warm-80">
       <div className="flex flex-col items-center justify-between py-5 text-center text-3xl">
-        <Icon
-          name="graduation-cap"
-          className="mb-5"
-          strokeWidth={2}
-          size={sizeIcon}
-        />
+        <Icon name={icon} className="mb-5" strokeWidth={2} size={sizeIcon} />
         <p>{title}</p>
       </div>
     </div>
