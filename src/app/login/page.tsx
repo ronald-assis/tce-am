60ac8c9bca1fec04354f4112dba9d7635c90eaac
ronald.assis <ronaldassis71@gmail.com>
2024-04-04 11:19:20 -0300

    feat(login): finish adjust layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,7 @@ import bgImage from '../../../public/assets/bg.png'
 export default function Login() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-blue_warm-80 to-blue_warm-30">
-      <div className="flex h-[644px] w-4/5 max-w-[960px] items-center justify-between rounded-lg bg-white shadow-2xl">
+      <div className="flex h-[560px] w-2/3 max-w-[960px] items-center justify-between rounded-lg bg-white shadow-2xl min-[1240px]:h-[644px] min-[1240px]:w-3/4 min-[1240px]:max-w-[960px]">
         <div className="h-full rounded-l-lg shadow-2xl">
           <Image
             alt="Imagem de fundo"
@@ -18,23 +18,23 @@ export default function Login() {
           />
         </div>
         <div className="flex h-full w-2/4 max-w-[450px] flex-col items-center rounded-r-lg bg-gradient-to-tl from-blue_warm-80 to-blue_warm-30 shadow-2xl">
-          <div className="mb-24 flex w-full justify-between">
-            <Image
-              alt="TCE-AM logo"
-              src={tceam}
-              width={300}
-              height={50}
-              priority
-              className="ml-2 mt-2 rounded-lg"
-            />
-            <Image
-              alt="RedData logo"
-              src={reddata}
-              width={100}
-              height={50}
-              priority
-              className="mr-2 mt-2 rounded-lg"
-            />
+          <div className="mb-24 flex w-full items-center justify-between p-3">
+            <div className="w-3/4">
+              <Image
+                alt="TCE-AM logo"
+                src={tceam}
+                priority
+                className="h-14 w-full rounded-lg"
+              />
+            </div>
+            <div className="w-1/5 max-w-[75px]">
+              <Image
+                alt="RedData logo"
+                src={reddata}
+                priority
+                className="h-14 w-full rounded-lg"
+              />
+            </div>
           </div>
           <Form />
         </div>
