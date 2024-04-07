d990cc0ac6d993cd3a9431fa05aaddee8a4122d2
ronald.assis <ronaldassis71@gmail.com>
2024-04-07 14:25:04 -0300

    feat(user): position div

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -8,19 +8,21 @@ export default function Perfil() {
   return (
     <>
       <Header title="Dados Pessoais" />
-      <main className="mt-40 grid min-h-screen grid-cols-2">
-        <div className="flex justify-center bg-red-500">
-          <div className="border-gray-300j flex h-12 w-full items-center gap-2 border-b-2">
-            <Button className="text-blue_warm-70">
-              <Link href="/">
-                <Icons name="tiHome" />
-              </Link>
-            </Button>
-            <Icons name="ioArrowRight" size={14} />
-            <span>Dados pessoais</span>
+      <main className="mt-40 flex justify-center">
+        <div className="grid min-h-screen w-2/3 grid-cols-2">
+          <div className="flex justify-center bg-red-500">
+            <div className="border-gray-300j flex h-12 w-full items-center gap-2 border-b-2">
+              <Button className="text-blue_warm-70">
+                <Link href="/">
+                  <Icons name="tiHome" />
+                </Link>
+              </Button>
+              <Icons name="ioArrowRight" size={14} />
+              <span>Dados pessoais</span>
+            </div>
           </div>
+          <div className="bg-yellow-600">permissões</div>
         </div>
-        <div className="bg-yellow-600">permissões</div>
       </main>
     </>
   )
