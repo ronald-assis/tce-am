ff5646acc6aaac785d2c9120488cc84417237757
ronald.assis <ronaldassis71@gmail.com>
2024-04-10 16:42:17 -0300

    feat(config) next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/tceam-dashboards',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/tceam-dashboards',
+        basePath: false,
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
