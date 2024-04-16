f1c49495f2f56c92dbe7f1aa700c5b4f54de2cc0
ronald.assis <ronaldassis71@gmail.com>
2024-04-16 18:36:03 -0300

    adjust in token

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,6 +7,6 @@ export const api = axios.create({
   baseURL: 'https://reddata.com.br/tceam-api/api/v1/',
   withCredentials: false,
   headers: {
-    Authorization: token ? `Bearer ${token}` : 'Bearer',
+    Authorization: token ? `Bearer ${token}` : '',
   },
 })
