1148709d8106a6bfe654f973a91b61b8fa8026ad
ronald.assis <ronaldassis71@gmail.com>
2024-04-25 14:36:41 -0300

    feat(dashboards): adding url

--- src/app/previsoes/meio_ambiente/[pathName]/page.tsx
+++ src/app/previsoes/meio_ambiente/[pathName]/page.tsx
@@ -1,11 +1,64 @@
 'use client'
+import { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation'
+import { AxiosError } from 'axios'
+import { DotLoader } from 'react-spinners'
+
+import { api } from '@/lib/api'
+import { getUser } from '@/lib/user'
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
+interface CategoryType {
+  id_categoria: string
+  nome_categoria: string
+  desc_categoria: string
+  itens_categoria: string
+  url_dashboard_simples: string
+  url_dashboard_completa: string
+}
+
+interface ResponseCategoriesType {
+  paginaAtual: number
+  registroPorPagina: number
+  conteudo: CategoryType[]
+}
+
 export default function Previsoes() {
+  const [loading, setLoading] = useState(false)
+  const [dashboardUrl, setDashboardUrl] = useState<string | undefined>('')
   const path = usePathname()
+
+  useEffect(() => {
+    const user = getUser()
+    setLoading(true)
+
+    if (user.admin === 1) {
+      api
+        .get('/categorias')
+        .then(({ data }) => {
+          const response: ResponseCategoriesType = data
+          const url = response.conteudo.find((c) =>
+            path.includes('qualidade')
+              ? c.itens_categoria.includes('Qualidade')
+              : path.includes('desmatamento') &&
+                c.itens_categoria.includes('Desmatamento'),
+          )
+          setDashboardUrl(url?.url_dashboard_completa)
+          setTimeout(() => {
+            setLoading(false)
+          }, 1700)
+        })
+        .catch((e) => {
+          const error = e as AxiosError | Error
+          if (error instanceof AxiosError) {
+            console.error(error.response?.data)
+          }
+        })
+    }
+  }, [path])
+
   let title = ''
   if (path === '/previsoes/meio_ambiente/qualidade_do_ar') {
     title = 'previsões > meio ambiente > qualidade do ar'
@@ -16,6 +69,21 @@ export default function Previsoes() {
   return (
     <>
       <Header title={title.toUpperCase()} />
+      <main className="mt-40 w-full ">
+        {loading && (
+          <DotLoader
+            className="left-1/2 top-40"
+            color="#3667d6"
+            size={100}
+            speedMultiplier={3}
+          />
+        )}
+        <iframe
+          className={`${loading ? 'hidden' : 'flex'} m-auto h-[44rem] w-11/12`}
+          src={dashboardUrl}
+          frameBorder="0"
+        ></iframe>
+      </main>
       <Footer />
     </>
   )
